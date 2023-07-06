8749eb403969e28353a6fb094904ded10bc00a9b
Nick Vicente <nicolas.a.vicente44@gmail.com>
2023-07-06 17:36:22 -0400

    Set up trending section and began coin detailsview

--- src/routes/CoinPage.jsx
+++ src/routes/CoinPage.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const CoinPage = () => {
+const [coin, setCoin] = useState({})
+
+const url =
+`https://api.coingecko.com/api/v3/coins/${params.coinId}?localization=false&sparkline=true`;
+
+useEffect(() => {
+  axios.get(url).then((response) => {
+    setCoin(response.data)
+   // console.log(response.data)
+  })
+}, [url])
+
   return (
-    <div>CoinPage</div>
+    <div>
+      <div>
+        <img src={coin.image?.large} alt="/" />
+        <div>
+          <p>{coin?.name} price</p>
+          <p>({coin.symbol?.toUpperCase()}/ USD)</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import CoinList from '../Components/CoinList'
+import Trending from '../Components/Trending'
 
 
 const Home = ({coins}) => {
   return (
     <div>
         <CoinList coins={coins}/>
+        <Trending/>
     </div>
   )
 }
